6715eeba796455b7a118ad633536cb85480a203f
Aidan Cuite <85179096+awccuite@users.noreply.github.com>
2025-10-24 12:29:16 -0400

    begin adding other system implementation options.

---
+++ ecs/systems/systems.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <chrono>
+
+// A system is a function that runs repeatedly on 
+// some timestep T (we can call this "ticks", = iter per second).
+
+// Want a system to be able to run on a separate thread (eg, physics runs on a thread)
+
+// Idea, define multiple types of system interfaces, base, Performance critical, simple lambda
+// Allow instantiation of systems to be flexible based on neccesary use cases for said systems.
+
+namespace gxe {
+    
+class System {
+public:
+    System(float tickrate) : 
+        _tickrate(tickrate), 
+        _tickInterval(1.0f / tickrate),
+        _accumulator(0),
+        _lastUpdate(std::chrono::steady_clock::now()) {};
+
+        virtual ~System() = default;
+
+        // called per frame
+        void update() {
+            auto now = std::chrono::steady_clock::now(); // Current time
+            auto deltaT = now - _lastUpdate; // Time since last tick
+            _lastUpdate = now;
+
+            _accumulator += deltaT.count(); // _accumulate.
+
+            while(_accumulator >= _tickInterval){
+                tick();
+                _accumulator--;
+            }
+        }
+
+        virtual void tick() = 0; // Tick method to be overridden in subclasses.
+
+private:
+    float _tickrate; // Ticks per second
+    float _tickInterval; // Seconds per tick.
+    float _accumulator; // Accumulated time since last tick
+    std::chrono::steady_clock::time_point _lastUpdate; // Last tick point.
+};
+
+// CRTP system implementation for performance-critical processes.
+template<typename Derived>
+class SystemCRTP {
+
+};
+
+// Provide super simple inline system interface intended for simple functionality
+class InlineSystem : public System {
+
+};
+
+} // namespace gxe
