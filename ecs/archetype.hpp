2ed6f18143985efd11c0b40885c20d3031b5ed97
Aidan Cuite <aidan@bantamtools.com>
2025-10-29 22:37:16 -0400

    Begin buildout of archetypes

--- ecs/archetype.hpp
+++ ecs/archetype.hpp
@@ -1,17 +1,34 @@
 #pragma once
 
+#include <cstdint>
+#include <limits>
+
 // Archetypes are templated with the components they are defined by.
 
 namespace gxe {
 
+using archetypeid = uint32_t;
+constexpr archetypeid NULL_ARCHETYPE = std::numeric_limits<archetypeid>::max();
+
 template<typename ...AComponents>
 class archetype {
+    static constexpr size_t N_COMPONENTS = sizeof...(AComponents);
+
 public:
+    // Archetypes can,
+    // addEntity
+    // removeEntity
+    // getEntityId
+    // getSwappedEntity
+    // getComponent
+    // forEach
+    // size
+    
+    // Added archetype ID's are just the sizeof components[0].
 
 private:
-    // Archetypes have a signature
-    // Archetypes have a mapping from type -> 
-
+    std::bitset<N_COMPONENTS> _signature; // Archetypes have a signature
+    std::tuple<std::vector<Components>...> _components; // Tuple of our components for this archetype
 };
 
 } // namespace gxe
